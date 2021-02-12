2b645d46d85ebe093dc3de3d0d69dbc302c9fccf
Andrzej Nowak <nnowakandrzej@gmail.com>
2021-02-12 12:30:19 +0100

    user interface

--- masterchef_backend/urls.py
+++ masterchef_backend/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from masterchef_backend.views import recipes_response, new, edit, delete
 
 urlpatterns = [
-    path('recipes/', recipes_response),
-    path('new/', new),
-    path('edit/<int:id>', edit),
-    path('delete/<int:id>', delete)
+    path('recipes/', recipes_response, name="all_recipe"),
+    path('new/', new, name="new_recipe"),
+    path('edit/<int:id>', edit, name="edit_recipe"),
+    path('delete/<int:id>', delete, name="delete_recipe")
 ]
